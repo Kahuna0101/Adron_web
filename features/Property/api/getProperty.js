63ae005a30b6cb099af646c6eaca69332f16f6cb
Dawodu Bolaji <dawodubolaji189@gmail.com>
2023-08-31 14:09:01 +0100

    new

--- features/Property/api/getProperty.js
+++
@@ -1,8 +0,0 @@
-import { axios } from "@/lib/axios";
-
-export const getProperty = async (id) => {
-    const { data } = await axios.get("/properties/detail", {
-        params: { externalID: id }
-    });
-    return data;
-};
